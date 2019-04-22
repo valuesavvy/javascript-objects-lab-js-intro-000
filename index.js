4fadbc2b7f650d8243ed60e72190d54b52d47fab
James Powell <nvrquitjp@gmail.com>
2019-04-22 13:23:36 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = {
+  flour: '2 cups',
+  eggs: 3,
+  butter: '1/2 stick'
+}
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  Object.assign({}, obj, {[ key]: value })
+}
+
